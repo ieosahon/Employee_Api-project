8ba2dd9f1685ccd417862cb441353905af7bdc76
Osahon Ighodaro <ieosahon@gmail.com>
2022-05-07 22:19:52 +0100

    Initial commit

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(opt =>
+           {
+               opt.AddPolicy("CorsPolicy", builder =>
+                   builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader()
+                   );
+           });
+
+        public static void ConfigureIISIntegration(this IServiceCollection servicess) =>
+            servicess.Configure<IISOptions>(opt =>
+            {
+
+            });
+    }
+}
