900d04058f1d4fad10ab27f6b86bf5e383017639
Osahon Ighodaro <ieosahon@gmail.com>
2022-05-11 23:22:06 +0100

    added repomanger class and interface

---
+++ Repository/RepoManager.cs
@@ -0,0 +1,29 @@
+﻿using Contracts;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class RepoManager : IRepoManager
+    {
+        private readonly RepoContext _repoContext;
+
+        public RepoManager(RepoContext repoContext)
+        {
+            _repoContext = repoContext;
+        }
+
+        public ICompanyRepo Company => throw new NotImplementedException();
+
+        public IEmployeeRepo Employee => throw new NotImplementedException();
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
