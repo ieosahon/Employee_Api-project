d78466a7645cab1e12748013eca81dd1453b5587
Osahon Ighodaro <ieosahon@gmail.com>
2022-05-11 22:57:25 +0100

    created an abstract class Repocabe class

--- Repository/RepoBase.cs
+++ Repository/RepoBase.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Contracts;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,28 @@ using System.Threading.Tasks;
 
 namespace Repository
 {
-    internal class RepositoryBase
+    public abstract class RepoBase<T> : IRepoBase<T> where T : class
     {
+        protected RepoContext _repoContext;
+
+        public RepoBase(RepoContext repoContext)
+        {
+            _repoContext = repoContext;
+        }
+
+        public IQueryable<T> FindAll(bool trackChanges) => !trackChanges ?
+            _repoContext.Set<T>().AsNoTracking() : _repoContext.Set<T>();
+
+        public IQueryable<T> FindByCondition(System.Linq.Expressions.Expression<Func<T, bool>> expression, bool trackChanges) => !trackChanges ? _repoContext.Set<T>().Where(expression).AsNoTracking()
+            : _repoContext.Set<T>()
+            .Where(expression);
+
+
+        public void Create(T entity) => _repoContext.Set<T>().Add(entity);
+
+        public void Delete(T entity) => _repoContext.Set<T>().Remove(entity);
+
+        public void Update(T entity) => _repoContext.Set<T>().Update(entity);
+
     }
 }
