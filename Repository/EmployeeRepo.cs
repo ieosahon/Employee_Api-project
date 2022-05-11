697d3b1b27d75f95ed6b8a312c13e6990c0558b4
Osahon Ighodaro <ieosahon@gmail.com>
2022-05-11 23:12:28 +0100

    added company and employee repo

---
+++ Repository/EmployeeRepo.cs
@@ -0,0 +1,19 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class EmployeeRepo : RepoBase<CompanyEmployee>, IEmployeeRepo
+    {
+        public EmployeeRepo(RepoContext repoContext) : base(repoContext)
+        {
+
+        }
+    }
+}
