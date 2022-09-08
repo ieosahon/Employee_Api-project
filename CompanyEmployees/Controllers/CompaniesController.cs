ae0a31376beb2b8720a03ad6bc46cf712b3bccc4
Osahon Ighodaro <ieosahon@gmail.com>
2022-09-08 16:23:51 +0100

    tested the get all compaines endpoint

--- CompanyEmployees/Controllers/CompaniesController.cs
+++ CompanyEmployees/Controllers/CompaniesController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Contracts;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace CompanyEmployees.Controllers
 {
@@ -7,5 +9,30 @@ namespace CompanyEmployees.Controllers
     [ApiController]
     public class CompaniesController : ControllerBase
     {
+        private readonly IRepoManager _manager;
+        private readonly ILoggerManager _logger;
+
+        public CompaniesController(ILoggerManager logger, IRepoManager manager)
+        {
+            _logger = logger;
+            _manager = manager;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllCompanies()
+        {
+            try
+            {
+                var companies = _manager.Company.GetAllCompanies(trackChanges: false);
+                return Ok(companies);
+
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError($"Something went wrong in {nameof(GetAllCompanies)} action {ex}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
     }
 }
